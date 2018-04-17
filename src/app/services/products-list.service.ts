ea2e1f509df87611926717cb27362ca41abaa705
Elyes Turki <elyestu@gmail.com>
2018-04-18 00:25:55 +0100

    add new data

--- src/app/services/products-list.service.ts
+++ src/app/services/products-list.service.ts
@@ -5,7 +5,8 @@ import { Product } from '../domain/product';
 export class ProductsListService {
    data : Product[] =  [ {code: 'P1000', nom: 'Café', prixUnitaire: 1500.4},
    {code: 'P2000', nom: 'the', prixUnitaire: 1600.6},
-   {code: 'P3000', nom: 'sucre', prixUnitaire: 1700.5}
+   {code: 'P3000', nom: 'sucre', prixUnitaire: 1700.5},
+   {code: 'P4000', nom: 'Coca Cola', prixUnitaire: 1800.5}
  ];
   constructor() { }
   getAllProducts(): Product[] {
