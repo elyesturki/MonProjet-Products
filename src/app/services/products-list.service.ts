3223ba54012a7599bb7f86fb7da5e1fe2abd5d65
Elyes Turki <elyestu@gmail.com>
2018-04-18 15:37:31 +0100

    add Observable service from json-server

--- src/app/services/products-list.service.ts
+++ src/app/services/products-list.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../domain/product';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
 
 @Injectable()
 export class ProductsListService {
-   data : Product[] =  [ {code: 'P1000', nom: 'Café', prixUnitaire: 1500.4},
+
+  URL: string = 'http://localhost:3000/products';
+   /*data : Product[] =  [ {code: 'P1000', nom: 'Café', prixUnitaire: 1500.4},
    {code: 'P2000', nom: 'the', prixUnitaire: 1600.6},
    {code: 'P3000', nom: 'sucre', prixUnitaire: 1700.5},
    {code: 'P4000', nom: 'Coca Cola', prixUnitaire: 1800.5}
- ];
-  constructor() { }
-  getAllProducts(): Product[] {
-    return this.data;
+ ];*/
+ constructor( private _service: HttpClient) { }
+ getAllProducts(): Observable<Product[] > {
+    return this._service.
+    get<Product[]>(this.URL);
   }
 }
