686c6e7873ca6650bff6af862049d67b25ca0007
Elyes Turki <elyestu@gmail.com>
2018-04-19 10:15:45 +0100

    Add Routing to navigate pages from navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -8,18 +9,28 @@ import { FooterComponent } from './components/footer/footer.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { ProductsListService } from './services/products-list.service';
 import { HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-
+const tableDeRoutage = [
+  { path: 'list', component: ProductsListComponent},
+  { path: 'welcome', component: WelcomeComponent},
+  { path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  { path: '**',  component: NotFoundComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    WelcomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(tableDeRoutage)
   ],
   providers: [ProductsListService],
   bootstrap: [AppComponent]
--- src/app/services/products-list.service.ts
+++ src/app/services/products-list.service.ts
@@ -8,11 +8,6 @@ import { Observable } from 'rxjs/Observable';
 export class ProductsListService {
 
   URL: string = 'http://localhost:3000/products';
-   /*data : Product[] =  [ {code: 'P1000', nom: 'Café', prixUnitaire: 1500.4},
-   {code: 'P2000', nom: 'the', prixUnitaire: 1600.6},
-   {code: 'P3000', nom: 'sucre', prixUnitaire: 1700.5},
-   {code: 'P4000', nom: 'Coca Cola', prixUnitaire: 1800.5}
- ];*/
  constructor( private _service: HttpClient) { }
  getAllProducts(): Observable<Product[] > {
     return this._service.
