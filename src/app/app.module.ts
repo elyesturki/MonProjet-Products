f25db34687dc69b368fff9b3ca3bce1599d58364
Elyes Turki <elyestu@gmail.com>
2018-04-19 13:11:25 +0100

    Add application multi-module and add separate module : MyRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -12,14 +10,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ProductsDetailComponent } from './components/products-detail/products-detail.component';
+import { MyRoutingModule } from './myRoutingModule/myrouting.module';
 
-const tableDeRoutage = [
-  { path: 'list', component: ProductsListComponent},
-  { path: 'welcome', component: WelcomeComponent},
-  { path: 'product/:id', component: ProductsDetailComponent},
-  { path: '', redirectTo: '/welcome', pathMatch: 'full'},
-  { path: '**',  component: NotFoundComponent}
-];
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,7 +25,7 @@ const tableDeRoutage = [
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(tableDeRoutage)
+    MyRoutingModule
   ],
   providers: [ProductsListService],
   bootstrap: [AppComponent]
